93ccc7b9624824db7074c8929bbd7f168cd38815
Jarjis Alam <jarjisalam19@gmail.com>
2026-06-06 18:59:03 +0530

    Fix dotenv import statement in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
-equire("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 const express = require("express");
 const cors = require("cors");
